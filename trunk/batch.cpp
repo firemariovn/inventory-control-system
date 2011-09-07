8ca194daef93d77f835589cddd6b893e4603d0e1
wdyabin@gmail.com <wdyabin@gmail.com@abd36893-8c2c-049e-f1ce-e78465f96fc8>
2011-09-07 18:06:13 +0000

    Update issue 9 Update class entity batch

---
+++ trunk/batch.cpp
@@ -0,0 +1,110 @@
+#include "batch.h"
+#include <QSqlTableModel>
+#include <QSqlQuery>
+#include <QDebug>
+#include <database.h>
+
+Batch::Batch()
+{
+}
+
+Batch::Batch(int id)
+{
+    QSqlQuery query;
+    query.exec("SELECT bid,gid,unitprice,quantity,expireddate, type, batchnumber,btime FROM Batch WHERE bid = :id");
+    query.bindValue(":id", id);
+    query.exec();
+    int gid = 0;
+    while (query.next())
+    {
+        this->bid = query.value(0).toInt();
+        gid = query.value(1).toInt();
+        this->unitPrice = query.value(2).toFloat();
+        this->quantity = query.value(3).toInt();
+        this->expiredDate = query.value(4).toDateTime();
+        this->type = query.value(5).toInt();
+        this->batchNumber = query.value(6).toString();
+        this->btime = query.value(7).toDateTime();
+    }
+}
+
+QSqlTableModel* Batch::getTableModel(){
+
+    QSqlTableModel *model = new QSqlTableModel();
+    model->setTable("Batch");
+    model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+    return model;
+}
+
+
+int Batch::getBid()
+{
+    return bid;
+}
+
+void Batch::setBid(int bid)
+{
+    this->bid = bid;
+}
+
+float Batch::getUnitPrice()
+{
+    return unitPrice;
+}
+
+void Batch::setUnitPrice(int unitPrice)
+{
+    this->unitPrice = unitPrice;
+}
+
+int Batch::getQuantity()
+{
+    return quantity;
+}
+
+void Batch::setQuantity(int quantity)
+{
+    this->quantity = quantity;
+}
+
+QDateTime Batch::getExpiredDate()
+{
+    return expiredDate;
+}
+
+void Batch::setExpiredDate(QDateTime expiredDate)
+{
+    this->expiredDate = expiredDate;
+}
+
+int Batch::getType()
+{
+    return type;
+}
+
+void Batch::setType(int type)
+{
+    this->type = type;
+}
+
+QString Batch::getBatchNumber()
+{
+    return batchNumber;
+}
+
+void Batch::setBatchNumber(QString batchNumber)
+{
+    this->batchNumber = batchNumber;
+}
+
+QDateTime Batch::getBTime()
+{
+    return btime;
+}
+
+void Batch::setBTime(QDateTime btime)
+{
+    this->btime = btime;
+}
+
+
