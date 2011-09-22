13044a0b80917821fb090c039eeefa96a0c710fb
zlw302323 <zlw302323@gmail.com>
2011-09-22 17:42:40 +0000

    modify login

--- icsloginform.cpp
+++ icsloginform.cpp
@@ -23,26 +23,48 @@ void ICSLoginForm::on_loginButton_clicked()
 {
     //Check if the user is authorized
     //createConnection();
+    bool position=true;
     Staff * loginer=new Staff();
     loginer->setName(ui->usrLineEdit->text());
     loginer->setPassword(ui->pwdLineEdit->text());
     if(ui->comboBox->currentIndex()==0)
     {
         loginer->ismanager=true;
+        if(!loginer->isManager())
+           {
+               //QMessageBox::warning(this,tr("Warning"),tr("please choose right position!"),QMessageBox::Yes);
+               position=false;
+           }
+
     }
     else
     {
         loginer->ismanager=false;
+        if(loginer->isManager())
+        {
+            //QMessageBox::warning(this,tr("Warning"),tr("please choose right position!"),QMessageBox::Yes);
+            position=false;
+        }
+
     }
-    if(loginer->login())
+    if(loginer->login()&&position)
         accept();
     else
+        if(!loginer->login())
 
-        QMessageBox::warning(this,tr("Warning"),tr("user name or password error!"),QMessageBox::Yes);
+        {
+
+            QMessageBox::warning(this,tr("Warning"),tr("user name or password error!"),QMessageBox::Yes);
         //clear all infomation of user label and password label
         ui->usrLineEdit->clear();
         ui->pwdLineEdit->clear();
         ui->usrLineEdit->setFocus();
+        }
+        else
+        {
+
+            QMessageBox::warning(this,tr("Warning"),tr("please choose right position!"),QMessageBox::Yes);
+        }
     }
 
 void ICSLoginForm::on_exitButton_clicked()
