31a750a791a4082d0320c1bef14745d5e8a88233
wdno01@gmail.com <wdno01@gmail.com@abd36893-8c2c-049e-f1ce-e78465f96fc8>
2011-11-01 19:36:49 +0000

    Update issue 39
    Add logging function to this project: add the library "log4j" to this project

---
+++ trunk/JVoter/src/main/java/org/ntu/eee/csn/oosd/jvoter/util/LogUtil.java
@@ -0,0 +1,16 @@
+package org.ntu.eee.csn.oosd.jvoter.util;
+
+import org.apache.log4j.Logger;
+
+
+
+public class LogUtil {
+	private static Logger LOGGER = Logger.getLogger(LogUtil.class);
+	public static void main(String[] args) {
+		for(int i =0;i < 100;i++){
+		LOGGER.warn("Testing Testing Testing Testing Testing Testing");
+		
+		}
+	}
+
+}
